a0df9195187fdaa93231782629b85a607feb58db
Chun Yu <junyussh@gmail.com>
2017-09-24 02:23:32 +0800

    Field exist finished Bug: function createUser needs async Todo: check API key

--- model/user.model.js
+++ model/user.model.js
@@ -8,7 +8,27 @@ var redisClient = redis.createClient({
 
 var user = {
     createUser: function(key, request, callback) {
-        redisClient.hset([config.database.key + "." + key, "username", request.username, "password", request.password, "key", request.key, "email", request.email, "address", request.address, "location", request.location], callback);
+    	redisClient.rpush([config.database.key+".user.id", request._id])
+    	redisClient.rpush([config.database.key+".user.username", request.username]);
+    	redisClient.rpush([config.database.key+".user.key", request.APIKey]);
+    	redisClient.rpush([config.database.key+".user.email", request.email]);
+        redisClient.hset([config.database.key + "." + key, "username", request.username, "password", request.password, "key", request.APIKey, "email", request.email], callback);
+    },
+    getAllUserField: function(field, callback) {
+    	redisClient.llen([config.database.key+".user."+field], function(err, length) {
+            redisClient.lrange([config.database.key+".user."+field, 0, length], callback);
+        });
+    },
+    ifUserFieldExist: function(field, _content) {
+    	this.getAllUserField(field, (err, field) => {
+    		for (var i = field.length - 1; i >= 0; i--) {
+    			if(field[i] == _content) {
+    				return true;
+    			} else {
+    				return false;
+    			}
+    		}
+    	});
     }
 }
 module.exports = user;
--- router/user.controller.js
+++ router/user.controller.js
@@ -4,35 +4,55 @@ var config = require("../config.json");
 var crypto = require("crypto");
 var uuid = require("uuid/v1");
 var APIKey = function(callback) {
-	var user = {};
-    var idBinary = (new Buffer(uuid()));
-    user.idBinary = idBinary.toString("utf8");
-    user._id = idBinary.toString('hex').substr(0, 8);
-    var keyBinary = new Buffer(uuid());
-    user.keyBinary = keyBinary.toString("utf8");
-    user.key = keyBinary.toString('base64').substr(0, 64);
+    var user = {};
+    do {
+        var idBinary = (new Buffer(uuid()));
+        user.idBinary = idBinary.toString("utf8");
+        user._id = idBinary.toString('hex').substr(0, 8);
+    } while (User.ifUserFieldExist("id", user._id));
+    do {
+        var keyBinary = new Buffer(uuid());
+        user.keyBinary = keyBinary.toString("utf8");
+        user.key = keyBinary.toString('base64').substr(0, 64);
+    } while (User.ifUserFieldExist("key", user._key))
     callback(user);
 };
 
 exports.vaildUser = (req, res) => {
-	res.json("Developing");
+    res.json("Developing");
 }
 exports.createUser = (req, res) => {
-	APIKey((keys)=> {
-		var info = {};
-		info._id = keys._id;
-		info.username = req.jsonBody.username;
-		info.email = req.jsonBody.email;
-		info.password = crypto.createHash('sha256').update(req.jsonBody.password).digest('base64');
-		info.address = req.jsonBody.address;
-		info.location = req.jsonBody.location;
-		info.APIKey = keys.key;
-		User.createUser(keys._id, info, (err, callback)=>{
-			if (!err) {
-				res.json({error: false, message: "User created"})
-			} else {
-				res.json({error: true, message: err});
-			}
-		})
-	})
+    var result = {};
+    APIKey((keys) => {
+        var info = {};
+        info._id = keys._id;
+        info.username = req.jsonBody.username;
+        info.email = req.jsonBody.email;
+        info.password = crypto.createHash('sha256').update(req.jsonBody.password).digest('base64');
+        info.APIKey = keys.key;
+        if (User.ifUserFieldExist("username", info.username)) {
+            result.error = true;
+            result.message = "Username existed!"
+        } else if (User.ifUserFieldExist("email", info.email)) {
+            result.error = true;
+            result.message = "Email existed!"
+        }
+        setTimeout(() => {
+        	console.log(result.error);
+            if (!result.error) {
+                User.createUser(keys._id, info, (err, callback) => {
+                    if (!err) {
+                        result.error = false;
+                        result.message = "User created";
+                    } else {
+                        result.error = true;
+                        result.message = err;
+                    }
+                })
+            }
+        }, 10);
+        setTimeout(() => {
+            res.json(result);
+        }, 30);
+    });
 }
